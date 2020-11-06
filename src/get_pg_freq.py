74b8c20ebbaf90f65df01f3b9cba4fbd4a69f304
Nikasha Patel <nikashap@gmail.com>
2020-11-06 12:09:29 -0500

    finalized calculation. stored result as

--- src/get_pg_freq.py
+++ src/get_pg_freq.py
@@ -2,8 +2,6 @@ import numpy as np
 import math as math
 import os as os
 
-
-'''need to calculate the frequency of p-g pairs given words in word_freq.txt'''
 # open word_freq.txt
 word_freq_file = open(os.getcwd()+"/data/word-freqs.txt")
 words = word_freq_file.read().splitlines()
@@ -14,7 +12,7 @@ word_freq = {}
 for word in words:
     w, f = word.split(' ')
     word_freq[w] = float(f)
-#print(word_freq['sevens'])
+print(sum(word_freq.values()))
 
 #open phonix.txt
 phonix_file = open(os.getcwd() + "/data/phonix.txt")
@@ -31,4 +29,17 @@ for word in words_phonemes:
 #find the words that are common to word_freq and to pg_rep
 shared_words = set(word_freq.keys()).intersection(set(pg_rep.keys()))
 
-#get the pg pairs of each word, multiply by the frequency of the word, and then output this somehow?
+#print(pg_rep['abalone'].split("|"))
+
+#Idea! split the shared words in pg_rep up by each `|` character to count how many of those specific pg pairs there are
+pg_freq = {}
+for word in shared_words:
+    pgs = pg_rep[word].split("|")
+    for pg in pgs:
+        pg_freq.setdefault(pg, 0)
+        pg_freq[pg] += word_freq[word] #check that this is the correct calculation
+
+#normalize the pg pair frequencies
+total_mass = sum(pg_freq.values())
+for pg in pg_freq.keys():
+    pg_freq[pg] /= total_mass
